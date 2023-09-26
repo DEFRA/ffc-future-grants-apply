0db07e5a06967ceb8908a353f35d63a56f11915a
diya2214 <madihakhn@yahoo.com>
2023-09-26 15:41:30 +0100

    clean form download js

--- app/routes/form-download.js
+++ app/routes/form-download.js
@@ -4,12 +4,11 @@ const { speciesRadios } = require('./models/form-component/species-radio')
 const session = require('../session')
 const urlPrefix = require('../config/index').urlPrefix
 
-const legendText = 'Which livestock do you want a review for?'
+const legendText = 'Download Application Form?'
 const errorText = 'Select the livestock type you want reviewed'
 const hintHtml = `<p>You can have one review every 10 months for one type of livestock.</p>
 <p>If you’re eligible for more than one type of livestock, you must choose which one you want reviewed.</p>`
 const backLink = `${urlPrefix}/org-review`
-const radioOptions = { isPageHeading: true, legendClasses: 'govuk-fieldset__legend--l', inline: false, hintHtml }
 
 module.exports = [
   {
@@ -18,7 +17,6 @@ module.exports = [
     options: {
       handler: async (request, h) => {
         return h.view('form-download', {
-          ...speciesRadios(legendText, whichReview, session.getFarmerApplyData(request, whichReview), undefined, radioOptions),
           backLink
         })
       }
@@ -30,11 +28,9 @@ module.exports = [
     options: {
       validate: {
         payload: Joi.object({
-          [whichReview]: Joi.string().valid('sheep', 'pigs', 'dairy', 'beef').required()
         }),
         failAction: (request, h, _err) => {
           return h.view('form-download', {
-            ...speciesRadios(legendText, whichReview, session.getFarmerApplyData(request, whichReview), errorText, radioOptions),
             backLink
           }).code(400).takeover()
         }
