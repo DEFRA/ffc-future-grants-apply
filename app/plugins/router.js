d94f1d506a0f9e82aaf25935fc575a64b9330e79
Ali Jahankah <aral2@yahoo.com>
2023-09-28 13:45:28 +0100

    fix- fix lint

--- app/plugins/router.js
+++ app/plugins/router.js
@@ -1,38 +1,38 @@
-const config = require("../config");
+const config = require('../config')
 
 const routes = [].concat(
-  require("../routes/accessibility"),
-  require("../routes/assets"),
-  require("../routes/cookies"),
-  require("../routes/guidance-for-farmers"),
-  require("../routes/healthy"),
-  require("../routes/healthz"),
-  require("../routes/index"),
-  require("../routes/org-review"),
-  require("../routes/privacy-policy"),
-  require("../routes/form-download"),
-  require("../routes/form-upload"),
-  require("../routes/not-eligible"),
-  require("../routes/check-answers"),
-  require("../routes/terms-and-conditions"),
-  require("../routes/signin-oidc")
-);
+  require('../routes/accessibility'),
+  require('../routes/assets'),
+  require('../routes/cookies'),
+  require('../routes/guidance-for-farmers'),
+  require('../routes/healthy'),
+  require('../routes/healthz'),
+  require('../routes/index'),
+  require('../routes/org-review'),
+  require('../routes/privacy-policy'),
+  require('../routes/form-download'),
+  require('../routes/form-upload'),
+  require('../routes/not-eligible'),
+  require('../routes/check-answers'),
+  require('../routes/terms-and-conditions'),
+  require('../routes/signin-oidc')
+)
 
 const registerYourInterestRoutes = [].concat(
-  require("../routes/register-your-interest/index"),
-  require("../routes/register-your-interest/registration-complete")
-);
+  require('../routes/register-your-interest/index'),
+  require('../routes/register-your-interest/registration-complete')
+)
 
 module.exports = {
   plugin: {
-    name: "router",
+    name: 'router',
     register: (server, _) => {
-      server.route(routes);
+      server.route(routes)
       if (config.registerYourInterest.enabled === true) {
-        server.route(registerYourInterestRoutes);
+        server.route(registerYourInterestRoutes)
       } else {
-        server.route(require("../routes/register-your-interest/index"));
+        server.route(require('../routes/register-your-interest/index'))
       }
-    },
-  },
-};
+    }
+  }
+}
--- app/routes/form-upload.js
+++ app/routes/form-upload.js
@@ -1,84 +1,84 @@
-const { urlPrefix, startPageUrl } = require("../config/index");
+const { urlPrefix, startPageUrl } = require('../config/index')
 
-const viewTemplate = "form-upload";
-const currentPath = `${urlPrefix}/${viewTemplate}`;
-const nextPath = startPageUrl;
-const { uploadFile } = require("../services/blob-storage"); // Import your uploadFile function
+const viewTemplate = 'form-upload'
+const currentPath = `${urlPrefix}/${viewTemplate}`
+const nextPath = startPageUrl
+const { uploadFile } = require('../services/blob-storage') // Import your uploadFile function
 
-function createModel(errorMessage) {
+function createModel (errorMessage) {
   return {
     formActionPage: currentPath,
     // Define multiple upload buttons, each with a unique ID and name
     applicationFormUploadButton: {
-      id: "application-form",
-      name: "applicationForm",
-      label: "Upload Application Form (DOC/DOCX)",
+      id: 'application-form',
+      name: 'applicationForm',
+      label: 'Upload Application Form (DOC/DOCX)'
     },
     appendixUploadButton: {
-      id: "appendix",
-      name: "appendix",
-      label: "Upload Appendix (XLS/XLSX)",
+      id: 'appendix',
+      name: 'appendix',
+      label: 'Upload Appendix (XLS/XLSX)'
     },
-    errorMessage,
-  };
+    errorMessage
+  }
 }
 
 module.exports = [
   {
-    method: "GET",
+    method: 'GET',
     path: currentPath,
     options: {
-      auth: false,
+      auth: false
     },
     handler: (request, h) => {
-      return h.view(viewTemplate, createModel(null));
-    },
+      return h.view(viewTemplate, createModel(null))
+    }
   },
   {
-    method: "POST",
+    method: 'POST',
     path: currentPath,
     options: {
       auth: false,
       payload: {
-        output: "stream",
+        output: 'stream',
         parse: true,
         multipart: true,
-        maxBytes: 20971520, // Max file size (e.g., 20MB)
-      },
+        maxBytes: 20971520 // Max file size (e.g., 20MB)
+      }
     },
     handler: async (request, h) => {
       try {
         // Get the uploaded file details
-        const applicationFormFile = request.payload["applicationForm"];
-        const appendixFile = request.payload["appendix"];
+        const applicationFormFile = request.payload.applicationForm
+        const appendixFile = request.payload.appendix
 
         if (!applicationFormFile && !appendixFile) {
           // No files were uploaded
           return h
-            .view(viewTemplate, createModel("No files selected"))
-            .takeover();
+            .view(viewTemplate, createModel('No files selected'))
+            .takeover()
         }
 
         // Upload the files to Azure Blob Storage (if they exist)
         if (applicationFormFile) {
-          const applicationFormBuffer = applicationFormFile._data;
-          const applicationFormFilename = applicationFormFile.hapi.filename;
-          await uploadFile(applicationFormBuffer, applicationFormFilename);
+          const applicationFormBuffer = applicationFormFile._data
+          const applicationFormFilename = applicationFormFile.hapi.filename
+          await uploadFile(applicationFormBuffer, applicationFormFilename)
         }
 
         if (appendixFile) {
-          const appendixBuffer = appendixFile._data;
-          const appendixFilename = appendixFile.hapi.filename;
-          await uploadFile(appendixBuffer, appendixFilename);
+          const appendixBuffer = appendixFile._data
+          const appendixFilename = appendixFile.hapi.filename
+          await uploadFile(appendixBuffer, appendixFilename)
         }
 
-        return h.redirect(nextPath);
+        return h.redirect(nextPath)
       } catch (error) {
-        console.error("Error uploading file(s):", error);
+        console.error('Error uploading file(s):', error)
         return h
-          .view(viewTemplate, createModel("Error uploading file(s)"))
-          .takeover();
+          .view(viewTemplate, createModel('Error uploading file(s)'))
+          .takeover()
       }
-    },
-  },
-];
+    }
+  }
+]
