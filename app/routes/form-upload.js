dc2c7baaa48b452f3603d89fdd3372cb14ea53f4
Ali Jahankah <aral2@yahoo.com>
2023-10-04 14:19:59 +0100

    feat- add download, delete and upload functionalities + tests passed

--- app/routes/form-upload.js
+++ app/routes/form-upload.js
@@ -1,25 +1,34 @@
-const { urlPrefix, startPageUrl } = require('../config/index')
+const { urlPrefix } = require('../config/index')
+const {
+  uploadFile,
+  deleteFile
+} = require('../services/blob-storage') // Import your uploadFile function
 
 const viewTemplate = 'form-upload'
 const currentPath = `${urlPrefix}/${viewTemplate}`
-const nextPath = startPageUrl
-const { uploadFile } = require('../services/blob-storage') // Import your uploadFile function
 
-function createModel (errorMessage) {
+function formatFileSize (bytes) {
+  const num = Number(bytes)
+  console.log(num, typeof num)
+  const megabytes = num / (1024 * 1024)
+  if (megabytes >= 1) {
+    return megabytes.toFixed(2) + ' MB'
+  } else {
+    const kilobytes = num / 1024
+    return kilobytes.toFixed(2) + ' KB'
+  }
+}
+function createModel (errorMessage, isClaimFormUploaded, file) {
   return {
     formActionPage: currentPath,
-    // Define multiple upload buttons, each with a unique ID and name
-    applicationFormUploadButton: {
-      id: 'applicationForm',
-      name: 'applicationForm',
-      label: 'Upload Application Form (DOC/DOCX)'
+    claimFormUploadButton: {
+      id: 'claimForm',
+      name: 'claimForm',
+      label: 'Upload Claim Form (DOC/DOCX)'
     },
-    appendixUploadButton: {
-      id: 'appendix',
-      name: 'appendix',
-      label: 'Upload Appendix (XLS/XLSX)'
-    },
-    errorMessage
+    errorMessage,
+    isClaimFormUploaded,
+    file
   }
 }
 
@@ -43,40 +52,68 @@ module.exports = [
         output: 'stream',
         parse: true,
         multipart: true,
-        maxBytes: 20971520 // Max file size (e.g., 20MB)
+        maxBytes: 20971520
       }
     },
     handler: async (request, h) => {
-      try {
-        const applicationFormFile = request.payload.applicationForm
-        const appendixFile = request.payload.appendix
-
-        if (!applicationFormFile && !appendixFile) {
-          // No files were uploaded
+      const { action } = request.payload
+      if (action === 'upload') {
+        console.log(request.payload)
+        try {
+          const claimFormFile = request.payload.claimForm
+          if (!claimFormFile) {
+            return h
+              .view(viewTemplate, createModel('No files selected'))
+              .takeover()
+          }
+          if (claimFormFile) {
+            const claimFormBuffer = claimFormFile._data
+            const claimFormFilename = claimFormFile.hapi.filename
+            const fileSizeBytes = claimFormBuffer.byteLength
+            const fileSizeFormatted = formatFileSize(fileSizeBytes)
+            const fileUploaded = await uploadFile(claimFormBuffer, claimFormFilename)
+            console.log(fileUploaded)
+            return h.view(
+              viewTemplate,
+              createModel(null, fileUploaded.isUploaded, {
+                originalFileName: fileUploaded.originalFileName,
+                fileSize: fileSizeFormatted,
+                fileName: fileUploaded.fileName
+              })
+            )
+          }
+        } catch (error) {
+          console.error('Error uploading file(s):', error)
           return h
-            .view(viewTemplate, createModel('No files selected'))
+            .view(
+              viewTemplate,
+              createModel('Error uploading file(s)', false, false)
+            )
             .takeover()
         }
-
-        // Upload the files to Azure Blob Storage (if they exist)
-        if (applicationFormFile) {
-          const applicationFormBuffer = applicationFormFile._data
-          const applicationFormFilename = applicationFormFile.hapi.filename
-          await uploadFile(applicationFormBuffer, applicationFormFilename)
+      }
+      if (action === 'delete') {
+        const fileName = request.payload.fileName
+        if (!fileName) {
+          console.log('No file name')
+          return h
+            .view(viewTemplate, createModel('Invalid file name', false, null))
+            .takeover()
         }
+        const isDeleted = await deleteFile(fileName)
 
-        if (appendixFile) {
-          const appendixBuffer = appendixFile._data
-          const appendixFilename = appendixFile.hapi.filename
-          await uploadFile(appendixBuffer, appendixFilename)
+        if (isDeleted) {
+          return h
+            .view(
+              viewTemplate,
+              createModel('File deleted successfully', false, null)
+            )
+            .takeover()
+        } else {
+          return h
+            .view(viewTemplate, createModel('Error deleting file', true, null))
+            .takeover()
         }
-
-        return h.redirect(nextPath)
-      } catch (error) {
-        console.error('Error uploading file(s):', error)
-        return h
-          .view(viewTemplate, createModel('Error uploading file(s)'))
-          .takeover()
       }
     }
   }
--- app/services/blob-storage.js
+++ app/services/blob-storage.js
@@ -1,5 +1,7 @@
 const { DefaultAzureCredential } = require('@azure/identity')
 const { BlobServiceClient } = require('@azure/storage-blob')
+// const { v4: uuidv4 } = require('uuid')
+
 const config = require('../config/blob-storage')
 let blobServiceClient
 
@@ -19,9 +21,40 @@ const blobContainerClient = blobServiceClient.getContainerClient(
 )
 
 async function uploadFile (buffer, filename) {
-  const blockBlobClient = blobContainerClient.getBlockBlobClient(filename)
-  await blockBlobClient.upload(buffer, buffer.byteLength)
-  console.log('Blob was uploaded successfully')
+  const filenameWithUuid = filename
+  try {
+    // const uuid = uuidv4()
+    const blockBlobClient = blobContainerClient.getBlockBlobClient(filenameWithUuid)
+    await blockBlobClient.upload(buffer, buffer.byteLength)
+    console.log('Blob was uploaded successfully')
+    return { fileName: filenameWithUuid, originalFileName: filename, isUploaded: true }
+  } catch (error) {
+    console.log(error)
+    return { fileName: filenameWithUuid, originalFileName: filename, isUploaded: false }
+  }
+}
+async function checkFileExists (fileName) {
+  try {
+    const blobClient = blobContainerClient.getBlobClient(fileName)
+    await blobClient.getProperties()
+    return true
+  } catch (error) {
+    if (error.statusCode === 404) {
+      return false
+    }
+    throw error
+  }
+}
+async function deleteFile (fileName) {
+  console.log(fileName)
+  try {
+    await blobContainerClient.getBlobClient(fileName).delete()
+    console.log(`Blob '${fileName}' was deleted successfully.`)
+    return true
+  } catch (error) {
+    console.error(`Error deleting blob '${fileName}':`, error)
+    return false
+  }
 }
 
-module.exports = { uploadFile }
+module.exports = { uploadFile, checkFileExists, deleteFile }
