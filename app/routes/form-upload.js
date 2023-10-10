5a98056bf8eb9a7e0dc3d82c1b3da155d4408289
Ali Jahankah <77119792+Ali-Jahankah@users.noreply.github.com>
2023-10-10 14:32:57 +0100

    Feat add error handling for upload (#5)
    * feat- add download, delete and upload functionalities + tests passe
    
    * ingress values
    
    ---------
    
    Co-authored-by: diya2214 <madihakhn@yahoo.com>

--- app/routes/form-upload.js
+++ app/routes/form-upload.js
@@ -1,25 +1,80 @@
-const { urlPrefix, startPageUrl } = require('../config/index')
+const Joi = require('joi')
 
+const { urlPrefix } = require('../config/index')
+const { uploadFile, deleteFile } = require('../services/blob-storage') // Import your uploadFile function
 const viewTemplate = 'form-upload'
 const currentPath = `${urlPrefix}/${viewTemplate}`
-const nextPath = startPageUrl
-const { uploadFile } = require('../services/blob-storage') // Import your uploadFile function
 
-function createModel (errorMessage) {
+function formatFileSize (bytes) {
+  const num = Number(bytes)
+  const megabytes = num / (1024 * 1024)
+  if (megabytes >= 1) {
+    return megabytes.toFixed(2) + ' MB'
+  } else {
+    const kilobytes = num / 1024
+    return kilobytes.toFixed(2) + ' KB'
+  }
+}
+function fileCheck (claimFormFile) {
+  const acceptableExtensions = [
+    'doc',
+    'docx',
+    'xls',
+    'xlsx',
+    'pdf',
+    'jpg',
+    'jpeg',
+    'png',
+    'mpg',
+    'mp4',
+    'wmv',
+    'mov'
+  ]
+  const claimFormFilename = claimFormFile.hapi.filename
+  const fileExtension = claimFormFilename.split('.').pop()
+  const isExtensionAllowed = acceptableExtensions.includes(fileExtension)
+  const allowedFileSize = 5000 * 1024
+  const claimFormBuffer = claimFormFile._data
+  const fileSizeBytes = claimFormBuffer.byteLength
+
+  const isAllowedSize = allowedFileSize >= Number(fileSizeBytes)
+  if (!claimFormFilename) {
+    return {
+      isCheckPassed: false,
+      errorMessage: 'No file selected. Select a file to upload.'
+    }
+  } else if (!isExtensionAllowed) {
+    return {
+      isCheckPassed: false,
+      errorMessage:
+        'The selected file must be a DOC, DOCX, XLS, XLSX, PDF, JPG, JPEG, PNG, MPG, MP4, WMV or MOV'
+    }
+  } else if (!isAllowedSize) {
+    return {
+      isCheckPassed: false,
+      errorMessage: 'The selected file must be smaller than 5MB'
+    }
+  } else {
+    const fileSizeFormatted = formatFileSize(fileSizeBytes)
+    return {
+      isCheckPassed: true,
+      fileBuffer: claimFormBuffer,
+      fileSizeFormatted,
+      claimFormFilename
+    }
+  }
+}
+function createModel (errorMessage, isClaimFormUploaded, file) {
   return {
     formActionPage: currentPath,
-    // Define multiple upload buttons, each with a unique ID and name
-    applicationFormUploadButton: {
-      id: 'applicationForm',
-      name: 'applicationForm',
-      label: 'Upload Application Form (DOC/DOCX)'
+    claimFormUploadButton: {
+      id: 'claimForm',
+      name: 'claimForm',
+      label: 'Upload Claim Form (DOC/DOCX)'
     },
-    appendixUploadButton: {
-      id: 'appendix',
-      name: 'appendix',
-      label: 'Upload Appendix (XLS/XLSX)'
-    },
-    errorMessage
+    errorMessage,
+    isClaimFormUploaded,
+    file
   }
 }
 
@@ -39,42 +94,139 @@ module.exports = [
     path: currentPath,
     options: {
       auth: false,
+      validate: {
+        payload: Joi.object({
+          claimForm: Joi.object({
+            hapi: Joi.object({
+              filename: Joi.string()
+                .regex(
+                  /\.(doc|docx|xls|xlsx|pdf|jpg|jpeg|png|mpg|mp4|wmv|mov)$/i
+                )
+                .required()
+            }).unknown()
+          }).unknown(),
+          action: Joi.string(),
+          fileName: Joi.string(),
+          fileDelete: Joi.object().unknown()
+        }),
+        failAction: async (request, h, error) => {
+          if (
+            error.output.payload.message.includes('match the required pattern')
+          ) {
+            return h
+              .view(
+                viewTemplate,
+                createModel(
+                  'The selected file must be a DOC, DOCX, XLS, XLSX, PDF, JPG, JPEG, PNG, MPG, MP4, WMV or MOV',
+                  false,
+                  null
+                )
+              )
+              .takeover()
+          } else if (error.output.payload.message.includes('empty')) {
+            return h
+              .view(
+                viewTemplate,
+                createModel('No file selected. Select a file to upload.')
+              )
+              .takeover()
+          } else {
+            return h
+              .view(
+                viewTemplate,
+                createModel(error.output.payload.message, false, null)
+              )
+              .takeover()
+          }
+        }
+      },
+
       payload: {
         output: 'stream',
         parse: true,
         multipart: true,
-        maxBytes: 20971520 // Max file size (e.g., 20MB)
+        maxBytes: 5000 * 1024,
+        failAction: async (request, h, error) => {
+          if (
+            error.output.payload.message.includes(
+              'Payload content length greater than maximum '
+            )
+          ) {
+            return h
+              .view(
+                viewTemplate,
+                createModel(
+                  'The selected file must be smaller than 5MB',
+                  false,
+                  null
+                )
+              )
+              .takeover()
+          }
+          return h
+            .view(viewTemplate, createModel(error, false, null))
+            .takeover()
+        }
       }
     },
     handler: async (request, h) => {
-      try {
-        const applicationFormFile = request.payload.applicationForm
-        const appendixFile = request.payload.appendix
-
-        if (!applicationFormFile && !appendixFile) {
+      const { action } = request.payload
+      if (action === 'upload') {
+        try {
+          const claimFormFile = request.payload.claimForm
+          const fileCheckDetails = fileCheck(claimFormFile)
+          if (fileCheckDetails.errorMessage) {
+            return h
+              .view(
+                viewTemplate,
+                createModel(fileCheckDetails.errorMessage, false)
+              )
+              .takeover()
+          } else {
+            const fileUploaded = await uploadFile(
+              fileCheckDetails.fileBuffer,
+              fileCheckDetails.claimFormFilename
+            )
+            return h.view(
+              viewTemplate,
+              createModel(null, fileUploaded.isUploaded, {
+                originalFileName: fileUploaded.originalFileName,
+                fileSize: fileCheckDetails.fileSizeFormatted,
+                fileName: fileUploaded.fileName
+              })
+            )
+          }
+        } catch (error) {
           return h
-            .view(viewTemplate, createModel('No files selected'))
+            .view(
+              viewTemplate,
+              createModel(
+                'The selected file could not be uploaded – try again',
+                false,
+                null
+              )
+            )
             .takeover()
         }
-        // Upload the files to Azure Blob Storage (if they exist)
-        if (applicationFormFile) {
-          const applicationFormBuffer = applicationFormFile._data
-          const applicationFormFilename = applicationFormFile.hapi.filename
-          await uploadFile(applicationFormBuffer, applicationFormFilename)
+      }
+      if (action === 'delete') {
+        const fileName = request.payload.fileName
+        if (!fileName) {
+          return h
+            .view(viewTemplate, createModel('Invalid file name', false, null))
+            .takeover()
         }
+        const isDeleted = await deleteFile(fileName)
 
-        if (appendixFile) {
-          const appendixBuffer = appendixFile._data
-          const appendixFilename = appendixFile.hapi.filename
-          await uploadFile(appendixBuffer, appendixFilename)
+        if (isDeleted) {
+          return h
+            .view(viewTemplate, createModel(null, false, null))
+            .takeover()
+        } else {
+          return h
+            .view(viewTemplate, createModel('Error deleting file', true, null))
+            .takeover()
         }
-
-        return h.redirect(nextPath)
-      } catch (error) {
-        console.error('Error uploading file(s):', error)
-        return h
-          .view(viewTemplate, createModel('Error uploading file(s)'))
-          .takeover()
       }
     }
   }
